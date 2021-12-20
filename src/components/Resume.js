3a9cd4550a7b168136bb9d07cca82241c27fea94
devhowey <dev.howey@gmail.com>
2021-12-19 20:57:43 -0600

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import "./Resume.scss";
 const Resume = () => {
     return (
         <div id="resume-section">
-            <a href="/" target={"blank"}>
+            <a href="https://docs.google.com/document/d/1QjyuPRpi8ocKlQXcXNTztdtz96um3ak85TK14s-BpS8/edit?usp=sharing" target={"blank"}>
                 <h3>howey's resume</h3>
             </a>
         </div>
