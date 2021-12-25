bf9fa9b36fe0b508b6a9008ce00a58c99e24571c
devhowey <dev.howey@gmail.com>
2021-12-25 12:05:51 -0600

    accessibility updates

--- src/stores/ProjectImgs.js
+++ src/stores/ProjectImgs.js
@@ -1,67 +1,67 @@
 export const FinCapImgs = [
     {
-        src: "https://i.ibb.co/NxVCwLQ/fincap-edit-transaction.png",
+        src: "https://lh3.googleusercontent.com/_KKzaWflNmATjSwIiHNAikIG9uXCEeM5BDNUsSmv9h9ptmc4bQpMyDlJ3iDXmUYbnQh27MKaJTwPK0e7lOC1RDs92gZIRwlzQakX1-y4iegjvf-hhBT8Lt5KD9bpmuxKF7pniXQlAA=w2400",
         alt: "fincap-edit-transaction",
     },
     {
-        src: "https://i.ibb.co/k0Nwyyb/fincap-full-app.png",
+        src: "https://lh3.googleusercontent.com/Bo-iNwa6_zDf-xFp5kr_ImCcc5fIBjy0qvGGuFxr6YeXBQMMcQWJFu0mFkv0kciKuw6hq7TkOeVmN4oKykAZQAkUsNddztRrG055Dw6t4g9M0j1xfHJE0Jx5XqgV-EEiT_ic_vqV-A=w2400",
         alt: "fincap-full-app",
     },
     {
-        src: "https://i.ibb.co/cgp005t/fincap-login.png",
+        src: "https://lh3.googleusercontent.com/0jKNT7it4kkYyu_QJoXNUASETqhKV77V1l3mW7FVXYgTeCbUlOMCXCc9OE6UMEx9o3VxEIVHsfJ1aPTP2sH7qdTZC5slAacSIcPG7z4Eci9IUS0P1ZQRK7VqeAvT_vfL0BAOlJ4pXw=w2400",
         alt: "fincap-login",
     },
     {
-        src: "https://i.ibb.co/6ydWN8T/fincap-register.png",
+        src: "https://lh3.googleusercontent.com/MSF3hC_8BNqDRUQxgy-ZZYMr-74RLzTzJ6vVVs1ZciAkHlbDjNtp6NoDBR7wolpkGZBR9022XpamI1Wd4tC5Vndq-h98_u5E6HrUMF9OPoOy5SwpF7T2NEquAIqTYFkiJ5hTMkxxng=w2400",
         alt: "fincap-register",
     }
 ];
 
 export const WayfarerImgs = [
     {
-        src: "https://i.ibb.co/28SjxHm/wayfarer-cities.png",
+        src: "https://lh3.googleusercontent.com/XJZ4KiApCMGqA_YrvJP_Qy0jYvJuQEtUie6fHK9LFA1P20jFXF4yoAtcc8TyPQZT0FmUuunif3Zb6tgk-1c7aZJ00XIbzIHxlvvREVskXYPbjy5bmwCgKVJgmYwmnjpuPYDmMMFtqQ=w2400",
         alt: "wayfarer-cities",
     },
     {
-        src: "https://i.ibb.co/WPg6J35/wayfarer-city-show.png",
+        src: "https://lh3.googleusercontent.com/YcYvbt_sBUdgueKh-BzVT2hCFKfrK0KPZe75QGrnztahs85lbOd0I1aFIPg2zE8d_rl_EO4aegaoTQwZHHCZ7TIRslpKdJMwj3M7Pu1NyB4RmXlvlZoz_PuAEjicTRN98rXCCNrAbQ=w2400",
         alt: "wayfarer-city-show",
     },
     {
-        src: "https://i.ibb.co/S3yMJmP/wayfarer-full-app.png",
+        src: "https://lh3.googleusercontent.com/AZyhbD2vPsNXLV7CFImrRBGZiD3tKfDZDfImVkDc4NApWo9vs5BHdo-4i6r0LC797RN0rAmLOw1bVrsMJ5cOw2FTf_Uum1MdtRNHOGOQcQxjaqzamJ7r6PGRw36g6ES-gyMjMo-T6w=w2400",
         alt: "wayfarer-full-app",
     },
     {
-        src: "https://i.ibb.co/GTbtscQ/wayfarer-profile.png",
+        src: "https://lh3.googleusercontent.com/iRrLIEvAgLt0omBvgP2J1bUaplALZLFvV0KLGsF7PD9VmXJEow5bJDUH3nnrsV5wVk6VLKCDFzQMFu7m2zu0ALzrkXEs5j06vsixtDX7qmUqKl4AOEngpYDwWkuYYlotHYMem_a7uw=w2400",
         alt: "wayfarer-profile",
     }
 ];
 
 export const pikChuImgs = [
     {
-        src: "https://i.ibb.co/kDMV87L/pikchu-login.png",
+        src: "https://lh3.googleusercontent.com/pJR2gtWQe0BOD4HYe8oEQrE1DRLz1fFyVmPWpBSxpOuuX03iBb6ImYzG-izDUI_dlNacFCYukFbS-wZHY9Aeew0FlE6NTaxB6KbmPkcxYnswMIyuDy1F2nd_RmRrFJD8mW8ozcoRTg=w2400",
         alt: "pikchu-login",
     },
     {
-        src: "https://i.ibb.co/kDMV87L/pikchu-login.png",
+        src: "https://lh3.googleusercontent.com/pJR2gtWQe0BOD4HYe8oEQrE1DRLz1fFyVmPWpBSxpOuuX03iBb6ImYzG-izDUI_dlNacFCYukFbS-wZHY9Aeew0FlE6NTaxB6KbmPkcxYnswMIyuDy1F2nd_RmRrFJD8mW8ozcoRTg=w2400",
         alt: "pikchu-login",
     },
 ];
 
 export const GotchiBuddyImgs = [
     {
-        src: "https://i.ibb.co/Wg8tgkk/gotchibuddy-gameplay.png",
-        alt: "seagull-stuff",
+        src: "https://lh3.googleusercontent.com/ZovU3f0w68IsVHFUMTlofg3G6LNNWhaEB1yAxUwv5E-QwAqvSjNI1BAAF-F3Wrv7lfzaKvfyzh5waM9bWklHALqQ3lvkseuyesT_219fdKqatarDLrmnbBXgEMoc0dxQ_6vZW8KPCg=w2400",
+        alt: "gotchibuddy-win",
     },
     {
-        src: "https://i.ibb.co/BNTcMsy/gotchibuddy-lostgame.png",
-        alt: "seagull-stuff",
+        src: "https://lh3.googleusercontent.com/WA6DUmMzqzgxYA4G5UHDRDY9sJC37uNbGDGptquEaVFtb5ZhR8jKeRWgO6PrpmVK06YW1G0FyDQnNRMJ1dSN3oiS9_6u36sgFpN-tr-qPeH_Zf7mk5ud9DgAsO9b7LntB9bqVnE92w=w2400",
+        alt: "gotchibuddy-die",
     },
     {
-        src: "https://i.ibb.co/59yk2d8/gotchibuddy-start.png",
-        alt: "seagull-stuff",
+        src: "https://lh3.googleusercontent.com/-MuHOA3VGeU9Gv5mpEiJIAbLPRsECkCeBucmwiWVEqJAH_OVSy-Y3r59DRDe7i0eXYqw4SIM_DEyq5GGn3I_CuKaowB9LwDEWQI1TpG1vc67KmxtSM_zsn2PgABc3WwpDy6pYCL-vw=w2400",
+        alt: "gotchibuddy-start",
     },
     {
-        src: "https://i.ibb.co/N9LHthF/gotchibuddy-wongame.png",
-        alt: "seagull-stuff",
+        src: "https://lh3.googleusercontent.com/3MVuUvmo6_OTNEwyglqxWt7BmD8hAT0d9QrRNTUhcxw1KzPjW5Z5PHO4lRG9W-kLb6VfnaRX1k9hFJUQEQOQTMIXRw1s4sw8MjTUwge-30dRl-tYMj8KP2t1EyYLQpI49L-bAuOAPw=w2400",
+        alt: "gotchibuddy-during-game",
     }
 ];
