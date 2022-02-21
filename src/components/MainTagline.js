148a7c71ec31ef19627d3c83fb12c05cda67c927
devhowey <dev.howey@gmail.com>
2022-02-21 17:19:10 -0600

    accessibility updates

--- src/components/MainTagline.js
+++ src/components/MainTagline.js
@@ -4,7 +4,7 @@ const MainTagline = () => {
     return (
         <div className="main-tagline-container">
             <h1 id="main__name"><a href="/" id="main__name__link">michael howey</a></h1>
-            <h2 id="main__title">remote frontend react developer</h2>
+            <h2 id="main__title">frontend developer</h2>
             <h3 id="main__tagline">
                 A Minnesotan in Mexico who enjoys finding patterns in syntax, both in spoken and programming languages  
             </h3>
